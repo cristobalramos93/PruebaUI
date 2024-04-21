0f29a019815ecf294ff1c76f84cc5044e878ef51
Cristobal Ramos <cristobal.ramos@vodafone.com>
2024-04-22 01:44:16 +0200

    Initial Commit

---
+++ PruebaUI/PruebaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaUIApp.swift
+//  PruebaUI
+//
+//  Created by Cristobal Ramos on 22/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

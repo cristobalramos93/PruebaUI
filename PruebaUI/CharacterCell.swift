8f74c690acbc842edb2f118a40233dcfe1eeeb1e
Cristobal Ramos <cristobal.ramos@vodafone.com>
2024-04-22 02:21:31 +0200

    first commit

---
+++ PruebaUI/CharacterCell.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterCell.swift
+//  PruebaUI
+//
+//  Created by Cristobal Ramos on 22/4/24.
+//
+
+import SwiftUI
+
+struct CharacterCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterCell()
+}
---
+++ PruebaUI/CharactersListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersListViewModel.swift
+//  PruebaUI
+//
+//  Created by Cristobal Ramos on 22/4/24.
+//
+
+import Foundation
